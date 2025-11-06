c523eb8f0e1349952356951bdd62e050a3934832
Ram Narayan Gundlapalli <ram@Rams-MacBook-Air.local>
2025-11-06 17:17:44 +0530

    Fix: prevent catch-all route from blocking API

--- server/server.js
+++ server/server.js
@@ -25,8 +25,10 @@ app.use('/api/judge', require('./routes/judgeRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  app.get('/*', (req, res) => {
+    if (!req.path.startsWith('/api')) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    }
   });
 }
 
